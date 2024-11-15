b4a1b68b9a1bb441a1d97a37d842493e6ad50341
FeliBlanco <felipeblanco09@hotmail.com>
2024-11-15 19:22:29 -0300

    dsd

--- index.js
+++ index.js
@@ -23,76 +23,60 @@ clientPS.connect().then(() => console.log('Conexión exitosa a PostgreSQL')).cat
 
 const PORT = process.env.PORT || 3000;
 
-const sistemas = [
-    {
-        id:2,
-        nombre: "Cobranzas front",
-        repositorio: "https://github.com/valentincabrera/cobranzas.front",
-        rama: "main",
-        ruta_final: "/home/proyectos/cobranzas/front",
-        directorio_copiar: "build",
-        type: 'front'
-    },
-    {
-        id:2,
-        nombre: "Gimnasio front",
-        repositorio: "https://github.com/valentincabrera/gimnasio.front",
-        rama: "main",
-        ruta_final: "/home/proyectos/gimnasio/front",
-        directorio_copiar: "build",
-        type: 'front'
-    },
-    {
-        id:3,
-        nombre: "Gimnasio back",
-        repositorio: "https://github.com/valentincabrera/gimnasio.back",
-        rama: "main",
-        ruta_final: "/home/proyectos/gimnasio/back",
-        directorio_copiar: "",
-        type: 'back'
-    }
-]
+let actualizando = []
 
 app.get('/actualizar', (req, res) => {
     const { sistema } = req.query;
 
     if(!sistema) return res.status(400).json({ error: 'Ingresa el ID del sistema' });
 
-    const sistemaIndex = sistemas.findIndex(i => i.id == sistema);
-    if(sistemaIndex != -1) {
-        const { repositorio, rama, ruta_final, directorio_copiar } = sistemas[sistemaIndex];
-        if(sistemas[sistemaIndex].type == "front")  {
-            exec(`bash /home/actualizadores/script.sh ${repositorio} ${rama} ${ruta_final} ${directorio_copiar}`, (error, stdout, stderr) => {
-                if(error) {
-                    console.error(`Error ejecutando el script: ${error.message}`);
-                    return res.status(500).json({ error: `Error: ${error.message}` });
-                }
-        
-                if(stderr) {
-                    console.warn(`Advertencias: ${stderr}`);
-                }
-                res.json({ output: stdout || stderr });
-            });
-        } else if(sistemas[sistemaIndex].type == "back") {
-            exec(`cd ${ruta_final} && git pull && docker-compose up --build -d`, (error, stdout, stderr) => {
-                if(error) {
-                    console.error(`Error ejecutando el script: ${error.message}`);
-                    return res.status(500).json({ error: `Error: ${error.message}` });
-                }
-        
-                if(stderr) {
-                    console.warn(`Advertencias: ${stderr}`);
-                }
-                res.json({ output: stdout || stderr });
-            });
-        } else {
-            res.status(503).send("No se encontró que tipo de sistema es")
-        }
+    clientPS.query(`SELECT * FROM proyectos WHERE id = $1`, [sistema]).then(response => {
+        if(response.rows.length > 0) {
+            const data = response.rows[0];
+            const { repositorio, rama, ruta_final, directorio_copiar } = data;
 
-    
-    } else {
-        res.status(503).send("No se encontró ningun sistema")
-    }
+            if(data.actualizando == 1) return res.status(401).send("Ya se está actualizando ese sistema")
+            if(data.type == "front")  {
+                clientPS.query(`UPDATE proyectos SET actualizando = 1 WHERE id = $1`, [data.id]);
+
+                exec(`bash /home/actualizadores/script.sh ${repositorio} ${rama} ${ruta_final} ${directorio_copiar}`, (error, stdout, stderr) => {
+                    if(error) {
+                        console.error(`Error ejecutando el script: ${error.message}`);
+                        clientPS.query(`UPDATE proyectos SET actualizando = 0 WHERE id = $1`, [data.id]);
+                        return res.status(500).json({ error: `Error: ${error.message}` });
+                    }
+            
+                    if(stderr) {
+                        console.warn(`Advertencias: ${stderr}`);
+                    }
+                    clientPS.query(`UPDATE proyectos SET actualizando = 0 WHERE id = $1`, [data.id]);
+                    res.json({ output: stdout || stderr });
+                });
+            } else if(data.type == "back") {
+                clientPS.query(`UPDATE proyectos SET actualizando = 1 WHERE id = $1`, [data.id]);
+                exec(`cd ${ruta_final} && git pull && docker-compose up --build -d`, (error, stdout, stderr) => {
+                    if(error) {
+                        console.error(`Error ejecutando el script: ${error.message}`);
+                        clientPS.query(`UPDATE proyectos SET actualizando = 0 WHERE id = $1`, [data.id]);
+                        return res.status(500).json({ error: `Error: ${error.message}` });
+                    }
+            
+                    if(stderr) {
+                        console.warn(`Advertencias: ${stderr}`);
+                    }
+                    clientPS.query(`UPDATE proyectos SET actualizando = 0 WHERE id = $1`, [data.id]);
+                    res.json({ output: stdout || stderr });
+                });
+            }
+        }
+        else {
+            res.status(503).send("No se encontró ningun proyecto")
+        }
+    })
+    .catch(err => {
+        res.status(503).send()
+    })
+
 });
 
 app.post('/proyecto', (req, res) => {
