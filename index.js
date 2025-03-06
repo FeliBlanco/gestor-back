09f3d3ae120dff97c905cfa77049964f02e65923
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-05 23:41:50 -0300

    ds

--- index.js
+++ index.js
@@ -376,6 +376,20 @@ app.get('/grupo/:grupo', (req, res) => {
     })
 })
 
+app.get('/proyectos/:grupo', async (req, res) => {
+    try {
+        const grupo = await clientPS.query(`SELECT * FROM grupos WHERE usuario = $1`, [req.params.grupo]);
+        if(grupo.rowCount == 0) return res.status(404).send("no")
+        const proyectos = await clientPS.query(`SELECT * FROM proyectos WHERE grupo = $1`, [grupo.rows[0].id])
+        res.send(proyectos.rows)
+        
+    }
+    catch(err) {
+        console.log(err)
+        res.status(503).send()
+    }
+})
+
 app.get('/proyecto/:grupo', (req, res) => {
     clientPS
     .query(`SELECT * FROM proyectos`)
