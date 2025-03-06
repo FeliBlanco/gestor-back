b9e4cf6eec722296b5ad895c2f9efd76fc44dc32
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-05 22:52:04 -0300

    dd

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.get('/actualizar/:id', async (req, res) => {
     -v ${URL_PROYECTOS}${grupo.rows[0].usuario}:/output \
     -w /app \
     node:18-alpine \
-    sh -c "npm install --legacy-peer-deps && npm run build && cp -r front /output"`, (error, stdout, stderr) => { 
+    sh -c "npm install --legacy-peer-deps && npm run build && cp -r dist /output/front"`, (error, stdout, stderr) => { 
             if(error) {
                 console.error(`Error ejecutando el script: ${error.message}`);
                 clientPS.query(`UPDATE proyectos SET actualizando = 0 WHERE id = $1`, [data.id]);
