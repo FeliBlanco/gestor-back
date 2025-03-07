0562296052453a33a173a877a16993c59f184c0c
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-06 21:45:29 -0300

    corregir framework

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.use('/proyecto', require('./proyecto/index.js'));
 router.use('/grupo', require('./grupo/index.js'));
-router.use('/framwork', require('./framework/index.js'));
+router.use('/framework', require('./framework/index.js'));
 router.use('/build', require('./build/index.js'));
 
 module.exports = router;
