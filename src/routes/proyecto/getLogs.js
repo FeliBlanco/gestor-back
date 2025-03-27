a80c53d0eb9949ce2a8b3e5e449558a457b20bc9
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-27 16:13:03 -0300

    dsd

--- src/routes/proyecto/getLogs.js
+++ src/routes/proyecto/getLogs.js
@@ -10,7 +10,7 @@ const getLogs = async (req, res) => {
     const proyecto = await clientPS.query(`SELECT proyect_directory FROM proyectos WHERE id = $1`, [project])
     if(proyecto.rowCount == 0) return res.status(404).send()
 
-    const logStream = spawn("docker", ["logs", "-f", data.proyect_directory.toLowerCase()]);
+    const logStream = spawn("docker", ["logs", "-f", proyecto.rows[0].proyect_directory.toLowerCase()]);
 
     logStream.stdout.on("data", (data) => {
         io.emit("log-project", data.toString());
