6accb90b7bde75fb534d4f18d68800073a71c003
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-27 13:10:31 -0300

    dsd

--- src/routes/proyecto/getProject.js
+++ src/routes/proyecto/getProject.js
@@ -11,19 +11,23 @@ const getProjectByGroupAndProjectId = async (req, res) => {
         const result = await clientPS.query(`SELECT P.*, D.dominio AS dominio FROM proyectos P LEFT JOIN dominios D ON D.proyecto_id = P.id WHERE P.usuario = $1 AND P.grupo = $2 LIMIT 1`, [proyecto, grupoData.rows[0].id])
         if(result.rowCount == 0) return res.status(404).send()
 
+            let status = "stopped"
             exec(`docker inspect ${result.rows[0].proyect_directory.toLowerCase()}`, (error, stdout, stderr) => {
                 if(stdout) {
-
                     try {
                         const data_docker = JSON.parse(stdout)
                         console.log(data_docker[0].State)
+                        status = data_docker[0].State.Status;
                     }
                     catch(err) {
-                        
+
                     }
                 }
+                res.send({
+                    ...result.rows[0],
+                    status
+                })
             })
-        res.send(result.rows[0])
     }
     catch(err) {
         console.log(err)
