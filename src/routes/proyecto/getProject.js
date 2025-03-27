3ef2858d9740f554079e2a87c9e10ed3a971b956
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-26 21:06:15 -0300

    dsd

--- src/routes/proyecto/getProject.js
+++ src/routes/proyecto/getProject.js
@@ -7,7 +7,7 @@ const getProjectByGroupAndProjectId = async (req, res) => {
         const grupoData = await clientPS.query(`SELECT * FROM grupos WHERE usuario = $1`, [grupo])
         if(grupoData.rowCount == 0) return res.status(404).send()
 
-        const result = await clientPS.query(`SELECT * FROM proyectos WHERE usuario = $1 AND grupo = $2`, [proyecto, grupoData.rows[0].id])
+        const result = await clientPS.query(`SELECT P.*, D.dominio AS dominio FROM proyectos P LEFT JOIN dominios D ON D.proyecto_id = P.id WHERE P.usuario = $1 AND P.grupo = $2 LIMIT 1`, [proyecto, grupoData.rows[0].id])
         if(result.rowCount == 0) return res.status(404).send()
         res.send(result.rows[0])
     }
