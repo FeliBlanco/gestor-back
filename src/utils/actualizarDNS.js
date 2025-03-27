feda974a106faf9ff3ae7db56037c7ce59abd8d7
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-27 12:57:38 -0300

    dsds

--- src/utils/actualizarDNS.js
+++ src/utils/actualizarDNS.js
@@ -8,15 +8,15 @@ const actualizarDNS = async (dominio_id) => {
         const response = await clientPS.query(`SELECT * FROM dominios WHERE id = $1`, [dominio_id])
         if(response.rowCount > 0) {
             const data = response.rows[0]
-            const envFilePath = path.join(`/etc/nginx/sites-available/`, data.dominio);
+            const envFilePath = path.join(`/etc/nginx/sites-available/`, data.dominio.toLowerCase());
 
             console.log("ACTUALIAR DNS")
             console.log(envFilePath)
 
-            const conf = data.configuracion.replace('[dominio]', data.dominio)
+            const conf = data.configuracion.replace('[dominio]', data.dominio.toLowerCase())
 
             await fs.writeFileSync(envFilePath, conf, 'utf8');
-            exec(`sudo ln -i /etc/nginx/sites-avaiable/${dominio} /etc/nginx/sites-enabled/`, async(error, stdout, stderr) => {
+            exec(`sudo ln -i /etc/nginx/sites-available/${dominio.toLowerCase()} /etc/nginx/sites-enabled/`, async(error, stdout, stderr) => {
 
             })
         }
