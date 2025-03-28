cb0386405167780837396327adc2098871ebe411
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-28 16:49:07 -0300

    dsds

--- src/routes/proyecto/index.js
+++ src/routes/proyecto/index.js
@@ -23,8 +23,8 @@ app.get('/build/:id', buildProject);
 app.get('/dominios/:proyecto', getDominios)
 app.get('/config/:proyecto', getConfig)
 app.get('/logs/:proyecto', getLogs)
-app.get('/start/:proyecto', startSystem)
-app.get('/stop/:proyecto', stopSystem)
+app.post('/start/:proyecto', startSystem)
+app.post('/stop/:proyecto', stopSystem)
 app.get('/:grupo/:proyecto', getProjectByGroupAndProjectId)
 
 app.get('/:grupo', getGroupProjects)
