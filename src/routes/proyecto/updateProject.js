e097fec358fa1e5dbbd39ddf954e99d4b6ad81f5
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-31 17:14:06 -0300

    dsd

--- src/routes/proyecto/updateProject.js
+++ src/routes/proyecto/updateProject.js
@@ -1,41 +1,54 @@
 const clientPS = require("../../db");
 
 
-const updateProject = (req, res) => {
-    const id = req.params.id;
-    const {
-        nombre,
-        repositorio,
-        rama,
-        ruta_final,
-        directorio_copiar,
-        type
-    } = req.body;
+const updateProject = async (req, res) => {
+    try {
 
-
-    let data = {};
-
-    if(nombre) data.nombre = nombre;
-    if(repositorio) data.repositorio = repositorio;
-    if(rama) data.rama = rama;
-    if(ruta_final) data.ruta_final = ruta_final;
-    if(directorio_copiar) data.directorio_copiar = directorio_copiar;
-    if(type) data.type = type;
-
-    let query = []
-    let values = []
-    Object.keys(data).forEach((value, index) => {
-        query.push(`${value} = $${index + 1}`)
-        values.push(data[value])
-    })
-
-    clientPS
-    .query(`UPDATE proyectos SET ${query.join(',')} WHERE id = ${id}`, values).then(res => {
+        const id = req.params.id;
+        const {
+            build_command,
+            install_command,
+            output_directory,
+            rama,
+            start_command,
+            env_vars
+        } = req.body;
+    
+    
+        let data = {};
+    
+        if(build_command) data.build_command = build_command;
+        if(install_command) data.install_command = install_command;
+        if(output_directory) data.output_directory = output_directory;
+        if(rama) data.rama = rama;
+        if(start_command) data.start_command = start_command;
+        
+        if(env_vars) {
+            await clientPS.query(`DELETE FROM env_vars WHERE proyecto = $1`, [id])
+            try {
+                env_vars.forEach(async env => {
+                    await clientPS.query(`INSERT INTO env_vars (key, value, proyecto) VALUES ($1, $2, $3)`, [env.key, env.value, id])
+                })
+            }
+            catch(err) {
+                console.log(err)
+            }
+        }
+    
+        let query = []
+        let values = []
+        Object.keys(data).forEach((value, index) => {
+            query.push(`${value} = $${index + 1}`)
+            values.push(data[value])
+        })
+    
+        await clientPS.query(`UPDATE proyectos SET ${query.join(',')} WHERE id = ${id}`, values)
         res.send()
-    })
-    .catch(err => {
+    }
+    catch(err) {
+        console.log(err)
         res.status(503).send()
-    })
+    }
 }
 
 module.exports = updateProject;
