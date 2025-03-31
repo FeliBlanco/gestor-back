1dc1504734b9e73d0bcb7a2b612e0b9adfd179f2
FeliBlanco <felipeblanco09@hotmail.com>
2025-03-31 13:29:51 -0300

    dasdsa

--- src/routes/proyecto/getConfig.js
+++ src/routes/proyecto/getConfig.js
@@ -3,9 +3,19 @@ const clientPS = require("../../db");
 const getConfig = async (req, res) => {
     try {
         const project_id = req.params.proyecto;
-        const result = await clientPS.query(`SELECT P.start_command, P.build_command, P.install_command, P.output_directory, P.repositorio, P.rama, P.framework, F.nombre AS framework, F.id AS framework_id FROM proyectos P LEFT JOIN frameworks F ON F.id = P.framework WHERE P.id = $1`, [project_id])
+        const result = await clientPS.query(`SELECT P.nombre, P.start_command, P.build_command, P.install_command, P.output_directory, P.repositorio, P.rama, P.framework, F.nombre AS framework, F.id AS framework_id FROM proyectos P LEFT JOIN frameworks F ON F.id = P.framework WHERE P.id = $1`, [project_id])
         if(result.rowCount == 0) return res.status(404).send()
-        res.send(result.rows[0])
+
+        let env_vars = []
+        const result_envs = await clientPS.query(`SELECT * FROM env_vars WHERE proyecto = $1`, [project_id])
+        if(result_envs.rowCount > 0) {
+            env_vars = result_envs.rows
+        }
+
+        res.send({
+            ...result.rows[0],
+            env_vars
+        })
     }
     catch(err) {
         console.log(err)
