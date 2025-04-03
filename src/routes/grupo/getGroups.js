921289cbdc96daed26636c4d2410eaca3210a0b8
FeliBlanco <felipeblanco09@hotmail.com>
2025-04-03 15:57:42 -0300

    dsd

--- src/routes/grupo/getGroups.js
+++ src/routes/grupo/getGroups.js
@@ -2,7 +2,7 @@ const clientPS = require("../../db");
 
 const getGroups = async (req, res) => {
     try {
-        const result = await clientPS.query(`SELECT * FROM grupos`, []);
+        const result = await clientPS.query(`SELECT *, (select count(id) FROM proyectos WHERE grupo = grupos.id) AS cantidadProyectos FROM grupos`, []);
         res.send(result.rows)
     }
     catch(err) {
--- src/routes/proyecto/getGroupProjects.js
+++ src/routes/proyecto/getGroupProjects.js
@@ -4,7 +4,8 @@ const getGroupProjects = async (req, res) => {
     try {
         const grupo = await clientPS.query(`SELECT * FROM grupos WHERE usuario = $1`, [req.params.grupo]);
         if(grupo.rowCount == 0) return res.status(404).send("no")
-        const proyectos = await clientPS.query(`SELECT * FROM proyectos WHERE grupo = $1`, [grupo.rows[0].id])
+
+        const proyectos = await clientPS.query(`SELECT P.*, D.dominio, F.nombre AS framework_nombre, F.tipo AS framework_tipo FROM proyectos P INNER JOIN frameworks F ON F.id = P.framework LEFT JOIN dominios D ON D.proyecto_id = P.id WHERE P.grupo = $1 ORDER BY P.id`, [grupo.rows[0].id])
         res.send(proyectos.rows)
 
     }
