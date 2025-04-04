698d0714be39a6dd70a792ce0b54274a1ddefe34
FeliBlanco <felipeblanco09@hotmail.com>
2025-04-04 20:12:09 -0300

    dsd

--- src/routes/proyecto/getConfig.js
+++ src/routes/proyecto/getConfig.js
@@ -3,7 +3,7 @@ const clientPS = require("../../db");
 const getConfig = async (req, res) => {
     try {
         const project_id = req.params.proyecto;
-        const result = await clientPS.query(`SELECT P.nombre, P.start_command, P.build_command, P.install_command, P.output_directory, P.repositorio, P.rama, P.framework, F.nombre AS framework, F.id AS framework_id FROM proyectos P LEFT JOIN frameworks F ON F.id = P.framework WHERE P.id = $1`, [project_id])
+        const result = await clientPS.query(`SELECT P.nombre, P.system_port, P.start_command, P.build_command, P.install_command, P.output_directory, P.repositorio, P.rama, P.framework, F.nombre AS framework, F.id AS framework_id FROM proyectos P LEFT JOIN frameworks F ON F.id = P.framework WHERE P.id = $1`, [project_id])
         if(result.rowCount == 0) return res.status(404).send()
 
         let env_vars = []
