96da98608dfa873e2886d1219bbd8ebb4d1a3cc0
FeliBlanco <felipeblanco09@hotmail.com>
2025-04-07 14:51:32 -0300

    edsd

--- src/utils/actualizarDNS.js
+++ src/utils/actualizarDNS.js
@@ -5,15 +5,21 @@ const { exec } = require('child_process');
 
 const actualizarDNS = async (dominio_id) => {
     try {
-        const response = await clientPS.query(`SELECT * FROM dominios WHERE id = $1`, [dominio_id])
+        const response = await clientPS.query(`SELECT D.*, G.usuario AS grupo_usuario, P.proyect_directory FROM dominios INNER JOIN proyectos P ON P.id = D.proyecto_id INNER JOIN grupos G ON G.id = P.grupo WHERE D.id = $1`, [dominio_id])
         if(response.rowCount > 0) {
+
             const data = response.rows[0]
             const envFilePath = path.join(`/etc/nginx/sites-available/`, data.dominio.toLowerCase());
 
             console.log("ACTUALIAR DNS")
             console.log(envFilePath)
 
-            const conf = data.configuracion.replace('[dominio]', data.dominio.toLowerCase())
+            let conf = data.configuracion.replaceAll('[dominio]', data.dominio.toLowerCase())
+
+            const directorio = `${global.URL_PROYECTOS}${data.grupo_usuario}/${data.proyect_directory}`
+
+            conf = data.configuracion.replaceAll('[directory]', directorio)
+
 
             await fs.writeFileSync(envFilePath, conf, 'utf8');
             exec(`sudo ln -i /etc/nginx/sites-available/${data.dominio.toLowerCase()} /etc/nginx/sites-enabled/ && sudo systemctl restart nginx`, async(error, stdout, stderr) => {
