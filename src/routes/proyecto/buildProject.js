e6d9fb8f3e189d3476c61e93b28ad763f5a27063
FeliBlanco <felipeblanco09@hotmail.com>
2025-04-09 21:55:22 -0300

    dsdsd

--- src/routes/proyecto/buildProject.js
+++ src/routes/proyecto/buildProject.js
@@ -69,7 +69,7 @@ const buildProject = async (req, res) => {
         && git checkout ${data.build_commit.length > 0 ? data.build_commit : rama} \
         ${envContent.length > 0 ? `&& echo '${envContent}' > /tmp/build_project2/.env` : ''} \
         && docker run --rm \
-    -v /tmp/build_project2:/build_input:ro \
+    -v /tmp/build_project2:/app \
     -v ${global.URL_PROYECTOS}${grupo.rows[0].usuario}:/output \
     -w /app \
     ${tipo_sistema_docker} \
